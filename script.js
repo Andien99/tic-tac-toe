d0d34bdecf76d8bde4d3b533607fa36046bfb425
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-05-06 09:21:03 +1000

    Coded the general tic tac toe logic that will check if a move is valid first then proceed to execute  and check if there are any winners

--- script.js
+++ script.js
@@ -3,71 +3,74 @@ const gameBoard = (function() {
     const playerSlots = []
     const CPUSlots = []
     console.log('testing')
-    let currentPlayer = 'Player'
-    function changePlayer() {
-        if (currentPlayer == 'Player') {
-            currentPlayer = 'CPU'
-            console.log('It is now CPU turn')
-        } else if (currentPlayer == 'CPU') {
-            currentPlayer = 'Player'
-            console.log('It is now Player turn')
-        }
-    }
+    let currentPlayer = 'CPU'
 
-    function moveSet (move) {
-        let currentLength = openSlots.length;
-        for (let i = 0; i < 9 ; i++) {
-            if (move == openSlots[i]) {
-                console.log('Player chose ' + openSlots[i] + ' where i is = ' + i)
-                if (currentPlayer == 'Player') {
-                playerSlots.push(openSlots[i])
-            }   else {
-                CPUSlots.push(openSlots[i])
-            }
-                openSlots.splice(i,1);   
-                console.log('availble slots are now ' + openSlots)
-                console.log('player chosen slots are ' + playerSlots)
-                console.log('CPU chosen slots are ' + CPUSlots)
-            }
-        }
-        if (currentLength == openSlots.length) {
+    function isValidMove(move) {
+        let chosenMove = move
+        if (openSlots.includes(chosenMove)) {
+            changePlayer();
+            moveSet(move);
+            checkIfWinner();
+        } else {
             console.log('this space is already taken!')
         }
+    
+            function changePlayer() {
+                if (currentPlayer == 'Player') {
+                    currentPlayer = 'CPU'
+                    console.log('It is now CPU turn')
+                } else if (currentPlayer == 'CPU') {
+                    currentPlayer = 'Player'
+                    console.log('It is now Player turn')
+                }
+            }
+    
+            function moveSet () {
+                for (let i = 0; i < 9 ; i++) {
+                    if (chosenMove == openSlots[i]) {
+                        console.log('Player chose ' + openSlots[i] + ' where i is = ' + i)
+                        if (currentPlayer == 'Player') {
+                        playerSlots.push(openSlots[i])
+                    }   else {
+                        CPUSlots.push(openSlots[i])
+                    }
+                        openSlots.splice(i,1);   
+                        console.log('availble slots are now ' + openSlots)
+                        console.log('player chosen slots are ' + playerSlots)
+                        console.log('CPU chosen slots are ' + CPUSlots)
+                    }
+                }
+            }
+    
+            function checkIfWinner() {
+                if (playerSlots.includes('s1') && playerSlots.includes('s2') && playerSlots.includes('s3')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('s4') && playerSlots.includes('s5') && playerSlots.includes('s6')) {
+                    console.log('player wins!')
+                } 
+                if (playerSlots.includes('s7') && playerSlots.includes('s8') && playerSlots.includes('s9')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('s1') && playerSlots.includes('s4') && playerSlots.includes('s7')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('s2') && playerSlots.includes('s5') && playerSlots.includes('s8')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('s3') && playerSlots.includes('s6') && playerSlots.includes('s9')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('s1') && playerSlots.includes('s5') && playerSlots.includes('s9')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('s3') && playerSlots.includes('s5') && playerSlots.includes('s7')) {
+                    console.log('player wins!')
+                }
+            }
     }
-
-    function checkIfWinner() {
-        if (playerSlots.includes('s1') && playerSlots.includes('s2') && playerSlots.includes('s3')) {
-            console.log('player wins!')
-        }
-        if (playerSlots.includes('s4') && playerSlots.includes('s5') && playerSlots.includes('s6')) {
-            console.log('player wins!')
-        } 
-        if (playerSlots.includes('s7') && playerSlots.includes('s8') && playerSlots.includes('s9')) {
-            console.log('player wins!')
-        }
-        if (playerSlots.includes('s1') && playerSlots.includes('s4') && playerSlots.includes('s7')) {
-            console.log('player wins!')
-        }
-        if (playerSlots.includes('s2') && playerSlots.includes('s5') && playerSlots.includes('s8')) {
-            console.log('player wins!')
-        }
-        if (playerSlots.includes('s3') && playerSlots.includes('s6') && playerSlots.includes('s9')) {
-            console.log('player wins!')
-        }
-        if (playerSlots.includes('s1') && playerSlots.includes('s5') && playerSlots.includes('s9')) {
-            console.log('player wins!')
-        }
-        if (playerSlots.includes('s3') && playerSlots.includes('s5') && playerSlots.includes('s7')) {
-            console.log('player wins!')
-        }
-    }
-
-return {
-    changePlayer,
-    moveSet,
-    checkIfWinner
-}
-})();
+return {isValidMove}
+})()
 
 function playerMove(move) {
     gameBoard.changePlayer(),
