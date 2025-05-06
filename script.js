575b143368f5fccacec1073475fd6f340d61b7fd
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-05-06 10:45:22 +1000

    Program will now console log a draw if there are no apparent winners and no spaces are available

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const gameBoard = (function() {
     const openSlots = ['1', '2', '3', '4', '5', '6','7', '8', '9'];
     const playerSlots = []
     const CPUSlots = []
-    console.log('testing')
+    let winner = ''
     let currentPlayer = 'CPU'
 
     function isValidMoveCPU () {
@@ -24,6 +24,27 @@ const gameBoard = (function() {
             moveSet(move);
             checkIfWinner();
             if (playerSlots.length !== CPUSlots.length) {
+                if (playerSlots.includes('4') && playerSlots.includes('5') && playerSlots.includes('6')) {
+                    console.log('player wins!')
+                } 
+                if (playerSlots.includes('7') && playerSlots.includes('8') && playerSlots.includes('9')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('1') && playerSlots.includes('4') && playerSlots.includes('7')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('2') && playerSlots.includes('5') && playerSlots.includes('8')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('3') && playerSlots.includes('6') && playerSlots.includes('9')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('1') && playerSlots.includes('5') && playerSlots.includes('9')) {
+                    console.log('player wins!')
+                }
+                if (playerSlots.includes('3') && playerSlots.includes('5') && playerSlots.includes('7')) {
+                    console.log('player wins!')
+                }
                 isValidMoveCPU()
             }
         } else {
@@ -58,29 +79,75 @@ const gameBoard = (function() {
             }
     
             function checkIfWinner() {
+                // Checks for Player's winning conditions
                 if (playerSlots.includes('1') && playerSlots.includes('2') && playerSlots.includes('3')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 }
                 if (playerSlots.includes('4') && playerSlots.includes('5') && playerSlots.includes('6')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 } 
                 if (playerSlots.includes('7') && playerSlots.includes('8') && playerSlots.includes('9')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 }
                 if (playerSlots.includes('1') && playerSlots.includes('4') && playerSlots.includes('7')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 }
                 if (playerSlots.includes('2') && playerSlots.includes('5') && playerSlots.includes('8')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 }
                 if (playerSlots.includes('3') && playerSlots.includes('6') && playerSlots.includes('9')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 }
                 if (playerSlots.includes('1') && playerSlots.includes('5') && playerSlots.includes('9')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
                 }
                 if (playerSlots.includes('3') && playerSlots.includes('5') && playerSlots.includes('7')) {
-                    console.log('player wins!')
+                    console.log('player wins!');
+                    winner = 'player'
+                }
+
+                // Checks for CPU's winning conditions
+                if (CPUSlots.includes('1') && CPUSlots.includes('2') && CPUSlots.includes('3')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (CPUSlots.includes('4') && CPUSlots.includes('5') && CPUSlots.includes('6')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                } 
+                if (CPUSlots.includes('7') && CPUSlots.includes('8') && CPUSlots.includes('9')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (CPUSlots.includes('1') && CPUSlots.includes('4') && CPUSlots.includes('7')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (CPUSlots.includes('2') && CPUSlots.includes('5') && CPUSlots.includes('8')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (CPUSlots.includes('3') && CPUSlots.includes('6') && CPUSlots.includes('9')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (CPUSlots.includes('1') && CPUSlots.includes('5') && CPUSlots.includes('9')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (CPUSlots.includes('3') && CPUSlots.includes('5') && CPUSlots.includes('7')) {
+                    console.log('CPU wins!');
+                    winner = 'CPU'
+                }
+                if (winner == '' && openSlots.length == 0) {
+                    console.log('Its a draw!')
                 }
             }
     }
