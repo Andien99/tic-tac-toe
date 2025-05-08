409258b9b15eb9d7b1ee83facd2214cf41cb29c9
Andien99 <170013021+Andien99@users.noreply.github.com>
2025-05-08 13:00:29 +1000

    created an interface for users to play tic tac toe

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
+const openSlots = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const allSlots = []
 const gameBoard = (function() {
-    const openSlots = ['1', '2', '3', '4', '5', '6','7', '8', '9'];
+
     const playerSlots = []
     const CPUSlots = []
     let winner = ''
     let currentPlayer = 'CPU'
+    const game = document.getElementById('game')
+
+    function settingUpBoard() {
+        for (let i = 1; i < 10; i++) {
+            let slot = document.createElement('div')
+            slot.classList.add('slot')
+            game.appendChild(slot)
+
+            let markers = document.createElement('p')
+            markers.id = i
+            slot.appendChild(markers)
+            allSlots.push(markers)
+
+            slot.addEventListener('click', () => {
+                isValidMove(i)
+                console.log(i)
+            })
+        }
+    }
+
 
     function isValidMoveCPU () {
         let randomSlot = ''
         function getRandomInt() {
             Math.floor(Math.random()*10)
-            randomSlot = (Math.floor(Math.random()*10)).toString()
+            randomSlot = (Math.floor(Math.random()*10))
         } 
         while (openSlots.includes(randomSlot) == false && openSlots.length > 0) {
             getRandomInt()
@@ -19,32 +41,12 @@ const gameBoard = (function() {
 
     function isValidMove(move) {
         let chosenMove = move
-        if (openSlots.includes(chosenMove)) {
+        if (openSlots.includes(move)) {
             changePlayer();
             moveSet(move);
             checkIfWinner();
-            if (playerSlots.length !== CPUSlots.length) {
-                if (playerSlots.includes('4') && playerSlots.includes('5') && playerSlots.includes('6')) {
-                    console.log('player wins!')
-                } 
-                if (playerSlots.includes('7') && playerSlots.includes('8') && playerSlots.includes('9')) {
-                    console.log('player wins!')
-                }
-                if (playerSlots.includes('1') && playerSlots.includes('4') && playerSlots.includes('7')) {
-                    console.log('player wins!')
-                }
-                if (playerSlots.includes('2') && playerSlots.includes('5') && playerSlots.includes('8')) {
-                    console.log('player wins!')
-                }
-                if (playerSlots.includes('3') && playerSlots.includes('6') && playerSlots.includes('9')) {
-                    console.log('player wins!')
-                }
-                if (playerSlots.includes('1') && playerSlots.includes('5') && playerSlots.includes('9')) {
-                    console.log('player wins!')
-                }
-                if (playerSlots.includes('3') && playerSlots.includes('5') && playerSlots.includes('7')) {
-                    console.log('player wins!')
-                }
+            
+            if (playerSlots.length !== CPUSlots.length && CPUSlots.length < 4) {
                 isValidMoveCPU()
             }
         } else {
@@ -67,10 +69,12 @@ const gameBoard = (function() {
                         console.log(currentPlayer  + ' chose ' + openSlots[i] + ' where i = ' + i)
                         if (currentPlayer == 'Player') {
                         playerSlots.push(openSlots[i])
+                        allSlots[i].textContent = 'o'
                     }   else {
-                        CPUSlots.push(openSlots[i])
+                        CPUSlots.push(openSlots[i]);
+                        allSlots[i].textContent = 'x'
                     }
-                        openSlots.splice(i,1);   
+                        openSlots[i]='taken';   
                         console.log('availble slots are now ' + openSlots)
                         console.log('player chosen slots are ' + playerSlots)
                         console.log('CPU chosen slots are ' + CPUSlots)
@@ -80,69 +84,69 @@ const gameBoard = (function() {
     
             function checkIfWinner() {
                 // Checks for Player's winning conditions
-                if (playerSlots.includes('1') && playerSlots.includes('2') && playerSlots.includes('3')) {
+                if (playerSlots.includes(1) && playerSlots.includes(2) && playerSlots.includes(3)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
-                if (playerSlots.includes('4') && playerSlots.includes('5') && playerSlots.includes('6')) {
+                if (playerSlots.includes(4) && playerSlots.includes(5) && playerSlots.includes(6)) {
                     console.log('player wins!');
                     winner = 'player'
                 } 
-                if (playerSlots.includes('7') && playerSlots.includes('8') && playerSlots.includes('9')) {
+                if (playerSlots.includes(7) && playerSlots.includes(8) && playerSlots.includes(9)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
-                if (playerSlots.includes('1') && playerSlots.includes('4') && playerSlots.includes('7')) {
+                if (playerSlots.includes(1) && playerSlots.includes(4) && playerSlots.includes(7)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
-                if (playerSlots.includes('2') && playerSlots.includes('5') && playerSlots.includes('8')) {
+                if (playerSlots.includes(2) && playerSlots.includes(5) && playerSlots.includes(8)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
-                if (playerSlots.includes('3') && playerSlots.includes('6') && playerSlots.includes('9')) {
+                if (playerSlots.includes(3) && playerSlots.includes(6) && playerSlots.includes(9)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
-                if (playerSlots.includes('1') && playerSlots.includes('5') && playerSlots.includes('9')) {
+                if (playerSlots.includes(1) && playerSlots.includes(5) && playerSlots.includes(9)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
-                if (playerSlots.includes('3') && playerSlots.includes('5') && playerSlots.includes('7')) {
+                if (playerSlots.includes(3) && playerSlots.includes(5) && playerSlots.includes(7)) {
                     console.log('player wins!');
                     winner = 'player'
                 }
 
                 // Checks for CPU's winning conditions
-                if (CPUSlots.includes('1') && CPUSlots.includes('2') && CPUSlots.includes('3')) {
+                if (CPUSlots.includes(1) && CPUSlots.includes(2) && CPUSlots.includes(3)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
-                if (CPUSlots.includes('4') && CPUSlots.includes('5') && CPUSlots.includes('6')) {
+                if (CPUSlots.includes(4) && CPUSlots.includes(5) && CPUSlots.includes(6)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 } 
-                if (CPUSlots.includes('7') && CPUSlots.includes('8') && CPUSlots.includes('9')) {
+                if (CPUSlots.includes(7) && CPUSlots.includes(8) && CPUSlots.includes(9)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
-                if (CPUSlots.includes('1') && CPUSlots.includes('4') && CPUSlots.includes('7')) {
+                if (CPUSlots.includes(1) && CPUSlots.includes(4) && CPUSlots.includes(7)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
-                if (CPUSlots.includes('2') && CPUSlots.includes('5') && CPUSlots.includes('8')) {
+                if (CPUSlots.includes(2) && CPUSlots.includes(5) && CPUSlots.includes(8)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
-                if (CPUSlots.includes('3') && CPUSlots.includes('6') && CPUSlots.includes('9')) {
+                if (CPUSlots.includes(3) && CPUSlots.includes(6) && CPUSlots.includes(9)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
-                if (CPUSlots.includes('1') && CPUSlots.includes('5') && CPUSlots.includes('9')) {
+                if (CPUSlots.includes(1) && CPUSlots.includes(5) && CPUSlots.includes(9)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
-                if (CPUSlots.includes('3') && CPUSlots.includes('5') && CPUSlots.includes('7')) {
+                if (CPUSlots.includes(3) && CPUSlots.includes(5) && CPUSlots.includes(7)) {
                     console.log('CPU wins!');
                     winner = 'CPU'
                 }
@@ -153,10 +157,15 @@ const gameBoard = (function() {
     }
 return {
     isValidMove, 
-    isValidMoveCPU
+    isValidMoveCPU,
+    settingUpBoard
 }
 })()
 
+
 function playerMove(move) {
+    
     gameBoard.isValidMove(move)
 }
+
+gameBoard.settingUpBoard()
